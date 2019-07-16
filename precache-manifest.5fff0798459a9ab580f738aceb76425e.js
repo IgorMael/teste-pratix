7737c1626dbbc5bf2fe229ad92ea6808b8030a47
Igor Mael <igormael@gmail.com>
2019-07-16 02:45:55 -0300

    Updates

---
+++ precache-manifest.5fff0798459a9ab580f738aceb76425e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9bb965b7061169f0c36d3b3db829f2a",
+    "url": "/teste-pratix/index.html"
+  },
+  {
+    "revision": "23828313c19c5c1bb0f9",
+    "url": "/teste-pratix/static/css/main.91cc4d2b.chunk.css"
+  },
+  {
+    "revision": "6b3147cba3a23d5d61ba",
+    "url": "/teste-pratix/static/js/2.5a525a24.chunk.js"
+  },
+  {
+    "revision": "23828313c19c5c1bb0f9",
+    "url": "/teste-pratix/static/js/main.a9db50d5.chunk.js"
+  },
+  {
+    "revision": "856f5a6cd9b83db6f764",
+    "url": "/teste-pratix/static/js/runtime~main.def73af0.js"
+  }
+]);
